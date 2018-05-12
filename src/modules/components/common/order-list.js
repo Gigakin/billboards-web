bc1b084129228970a3715f17bea75a8398b9aacc
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-05-12 13:22:12 +0530

    added order list comoponent

--- src/modules/components/common/order-list.js
+++ src/modules/components/common/order-list.js
@@ -4,7 +4,30 @@ import React from "react";
 // Classes
 class OrderList extends React.Component {
   render() {
-    return "Order List";
+    return (
+      <div className="order-list">
+        <table className="uk-table uk-table-small uk-table-divider">
+          <thead>
+            <tr>
+              <th>Party Name</th>
+              <th>Job Name</th>
+              <th>Total Sq. Ft.</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Table Data</td>
+              <td>Table Data</td>
+              <td>Table Data</td>
+              <td>Table Data</td>
+              <td>Table Data</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
