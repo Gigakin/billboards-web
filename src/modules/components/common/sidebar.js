a115c0c5f2359c4ea871135183d008d256815bbc
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-05-17 22:05:20 +0530

    added icon to sidebar menu

--- src/modules/components/common/sidebar.js
+++ src/modules/components/common/sidebar.js
@@ -4,20 +4,50 @@ import { Link } from "react-router-dom";
 
 // Classes
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmenuVisible: true
+    };
+  }
+
+  toggleSubmenu = () => {
+    let { isSubmenuVisible } = this.state;
+    return this.setState({ isSubmenuVisible: !isSubmenuVisible });
+  };
+
   render() {
+    let { isSubmenuVisible } = this.state;
     return (
       <div className="sidebar">
         <div className="sidebar__menu">
           <Link to="/dashboard" className="sidebar__menu__link">
-            Dashboard
+            <span uk-icon="thumbnails" /> Dashboard
           </Link>
-          <div className="sidebar__menu__section-title">Job Management</div>
-          <Link to="/jobs" className="sidebar__menu__link">
-            Orders List
+          <Link
+            to="#"
+            className="sidebar__menu__link"
+            onClick={this.toggleSubmenu}
+          >
+            <span uk-icon="tag" /> Job Management
           </Link>
-          <Link to="/jobs/new" className="sidebar__menu__link">
-            Create New Job
-          </Link>
+          {/* Submenu */}
+          {isSubmenuVisible ? (
+            <div>
+              <Link
+                to="/jobs"
+                className="sidebar__menu__link sidebar__menu__link--indented"
+              >
+                <span uk-icon="chevron-right" />Orders List
+              </Link>
+              <Link
+                to="/jobs/new"
+                className="sidebar__menu__link sidebar__menu__link--indented"
+              >
+                <span uk-icon="chevron-right" />Create New Job
+              </Link>
+            </div>
+          ) : null}
         </div>
       </div>
     );
