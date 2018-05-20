5ae16f1ba27fb810c0636688a9c4d0b16b2564ab
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-05-20 12:42:37 +0530

    added bills page and bills table with dummy data

---
+++ src/modules/components/common/bills-table.js
@@ -0,0 +1,186 @@
+/*
+  Component: Bills Table
+  Props: columns (array)
+         data (array)
+*/
+
+// Modules
+import React from "react";
+import { withRouter } from "react-router-dom";
+import Methods from "../../methods";
+
+// Classes
+class BillsTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: props.data,
+      currentPage: 0
+    };
+    this.pageSize = 10;
+    this.originalList = Methods.clone(props.data);
+    this.filteredList = [];
+  }
+
+  // Filter Table Data
+  filterData = event => {
+    // Extract values
+    let query = event.target.value;
+    let filterBy = event.target.id;
+
+    // Empty query
+    // assing original list to state
+    if (!query) {
+      return this.setState({
+        tableData: this.originalList
+      });
+    }
+
+    // No "FilterBy" property
+    // Assign the default manually
+    if (!filterBy) filterBy = "id";
+
+    // Find Entries and assign
+    // them to filtered list
+    this.filteredList = [];
+    this.originalList.filter(item => {
+      if (item[filterBy]) {
+        query = query.toLowerCase();
+        if (item[filterBy].toLowerCase().includes(query)) {
+          return this.filteredList.push(item);
+        }
+      }
+      return false;
+    });
+
+    // Update results
+    return this.setState({ tableData: this.filteredList });
+  };
+
+  // View Bill
+  viewBill = billid => {
+    if (billid) return this.props.history.push(`/bills/${billid}`);
+  };
+
+  // Send Reminder
+  sendReminder = billid => {
+    if (billid) return console.log(`Reminder sent for Bill #${billid}`);
+  };
+
+  render() {
+    let { columns } = this.props;
+    let { tableData } = this.state;
+    return (
+      <div className="sor-table">
+        {/* Actions */}
+        <div className="sor-table__actions">
+          <div className="uk-grid uk-grid-small uk-form-stacked">
+            <div className="uk-width-1-3">
+              <label className="uk-form-label">Search by Party Name</label>
+              <div className="uk-form-controls">
+                <input
+                  id="party"
+                  type="text"
+                  onChange={this.filterData}
+                  className="uk-input"
+                />
+              </div>
+            </div>
+            <div className="uk-width-1-3">
+              <label className="uk-form-label">Search by Job Name</label>
+              <div className="uk-form-controls">
+                <input
+                  id="job"
+                  type="text"
+                  onChange={this.filterData}
+                  className="uk-input"
+                />
+              </div>
+            </div>
+            <div className="uk-width-1-3">
+              <label className="uk-form-label">Search by Job Status</label>
+              <div className="uk-form-controls">
+                <select
+                  id="searchJobStatus"
+                  onChange={this.filterData}
+                  className="uk-select"
+                >
+                  <option defaultChecked />
+                  <option value="ready">Ready for Billing</option>
+                  <option value="waitlisted">Waiting for Pickup</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Table */}
+        <div className="sor-table__table">
+          <table className="uk-table uk-table-striped uk-table-divider">
+            <thead>
+              <tr>
+                {columns && columns.length
+                  ? columns.map((item, index) => (
+                      <th key={`tableheading_${index}`}>{item}</th>
+                    ))
+                  : null}
+              </tr>
+            </thead>
+            <tbody>
+              {tableData && tableData.length ? (
+                tableData.map((item, index) => (
+                  <tr key={`sortable_item_${index}`}>
+                    <td>{item.id}</td>
+                    <td>{item.party}</td>
+                    <td>{item.job}</td>
+                    <td>{item.date ? item.date : "1/1/2000"}</td>
+                    <td>
+                      <span>
+                        <button
+                          type="button"
+                          className="uk-button uk-margin-small-right uk-button-link uk-text-primary"
+                          onClick={() => this.viewBill(item.id)}
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="uk-button uk-margin-small-right uk-button-link uk-text-primary"
+                          onClick={() => this.sendReminder(item.id)}
+                        >
+                          Send Reminder
+                        </button>
+                      </span>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={columns.length}>
+                    <span className="uk-padding">
+                      <div className="uk-text-center">
+                        Nothing here!<br />
+                        <div className="uk-text-meta uk-margin-small-top">
+                          Looking for something? Try using more specific
+                          keywords.
+                        </div>
+                      </div>
+                    </span>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+// Default Props
+BillsTable.defaultProp = {
+  columns: {},
+  data: []
+};
+
+// Exports
+export default withRouter(BillsTable);
--- src/modules/components/pages/bills.js
+++ src/modules/components/pages/bills.js
@@ -1,10 +1,37 @@
 // Modules
 import React from "react";
 
+// Components
+import BillsTable from "../common/bills-table";
+import MockData from "../../stores/mock_data.json";
+
 // Classes
 class Bills extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      billsList: MockData
+    };
+    this.tableColumns = ["#", "Party Name", "Job Name", "Job Date", "Actions"];
+  }
+
   render() {
-    return "Bills";
+    let { billsList } = this.state;
+    return (
+      <div className="uk-flex">
+        <div className="uk-width-1-1 uk-padding-large">
+          {/* Header */}
+          <div className="uk-width-1-1">
+            <h2>Bills</h2>
+          </div>
+
+          {/* Bills */}
+          <div className="uk-width-1-1">
+            <BillsTable columns={this.tableColumns} data={billsList} />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
