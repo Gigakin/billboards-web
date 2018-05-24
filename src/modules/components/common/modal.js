7bd11b03f04bca7695bd85e72b2fc1e0facaf771
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-05-24 22:32:34 +0530

    added modal component using react-modal

---
+++ src/modules/components/common/modal.js
@@ -0,0 +1,13 @@
+// Modules
+import React from "react";
+import ReactModal from "react-modal";
+
+// Classes
+class Modal extends React.Component {
+  render() {
+    return <ReactModal {...this.props} />;
+  }
+}
+
+// Exports
+export default Modal;
