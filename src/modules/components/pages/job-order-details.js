21d5ca3cb9a13cba7afa873c4f8710d225917fe3
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-05-27 13:48:52 +0530

    added example jobs in order details page

--- src/modules/components/pages/job-order-details.js
+++ src/modules/components/pages/job-order-details.js
@@ -155,6 +155,162 @@ class JobOrderDetails extends React.Component {
                     </div>
                   </div>
                 </div>
+
+                <div className="order-details__jobs-list__item">
+                  <div className="uk-text-lead order-details__jobs-list__item__title">
+                    Indoor
+                  </div>
+                  <div className="uk-grid uk-grid-small uk-margin-small">
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Type : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        Indoor
+                      </span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Quality : </span>
+                      <span className="uk-text-small uk-text-primary">Eco</span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Quantity : </span>
+                      <span className="uk-text-small uk-text-primary">2</span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Dimensions : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        120 x 165 sq. ft.
+                      </span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Other Features : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        Matte Lamination
+                      </span>
+                    </div>
+                  </div>
+                  <div className="uk-width-1-1 uk-margin-top">
+                    <div className="uk-flex uk-flex-between">
+                      {/* Downloads */}
+                      <div className="uk-flex">
+                        {permissions.canDownloadCustomerDesignFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-download" /> Download Customer
+                            Design File
+                          </button>
+                        ) : null}
+                        {permissions.canDownloadDesignerDesignFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-download" /> Download Design
+                            File
+                          </button>
+                        ) : null}
+                        {permissions.canAttachDesignFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-upload" /> Attach Design File
+                          </button>
+                        ) : null}
+                        {permissions.canAttachPrintFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-upload" /> Attach REAP File
+                          </button>
+                        ) : null}
+                      </div>
+
+                      {/* Complete Job */}
+                      <div className="uk-flex">
+                        {permissions.canSendForPrinting ? (
+                          <button className="uk-button uk-button-small uk-button-secondary">
+                            <span uk-icon="check" /> Designing Complete
+                          </button>
+                        ) : null}
+                        {permissions.canMarkAsPrintingDone ? (
+                          <button className="uk-button uk-button-small uk-button-secondary">
+                            <span uk-icon="check" /> Printing Complete
+                          </button>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="order-details__jobs-list__item">
+                  <div className="uk-text-lead order-details__jobs-list__item__title">
+                    Front-lit
+                  </div>
+                  <div className="uk-grid uk-grid-small uk-margin-small">
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Type : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        Front-lit
+                      </span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Quality : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        Banner
+                      </span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Quantity : </span>
+                      <span className="uk-text-small uk-text-primary">2</span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Dimensions : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        120 x 165 sq. ft.
+                      </span>
+                    </div>
+                    <div className="uk-width-1-2">
+                      <span className="uk-text-small">Other Features : </span>
+                      <span className="uk-text-small uk-text-primary">
+                        Framing
+                      </span>
+                    </div>
+                  </div>
+                  <div className="uk-width-1-1 uk-margin-top">
+                    <div className="uk-flex uk-flex-between">
+                      {/* Downloads */}
+                      <div className="uk-flex">
+                        {permissions.canDownloadCustomerDesignFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-download" /> Download Customer
+                            Design File
+                          </button>
+                        ) : null}
+                        {permissions.canDownloadDesignerDesignFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-download" /> Download Design
+                            File
+                          </button>
+                        ) : null}
+                        {permissions.canAttachDesignFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-upload" /> Attach Design File
+                          </button>
+                        ) : null}
+                        {permissions.canAttachPrintFile ? (
+                          <button className="uk-button uk-button-small uk-button-default uk-margin-small-right">
+                            <span uk-icon="cloud-upload" /> Attach REAP File
+                          </button>
+                        ) : null}
+                      </div>
+
+                      {/* Complete Job */}
+                      <div className="uk-flex">
+                        {permissions.canSendForPrinting ? (
+                          <button className="uk-button uk-button-small uk-button-secondary">
+                            <span uk-icon="check" /> Designing Complete
+                          </button>
+                        ) : null}
+                        {permissions.canMarkAsPrintingDone ? (
+                          <button className="uk-button uk-button-small uk-button-secondary">
+                            <span uk-icon="check" /> Printing Complete
+                          </button>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="order-details__footer">
                 <div className="uk-flex uk-flex-between">
