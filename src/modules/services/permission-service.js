c48f70f7f57f0e85a05c2f18f90fe3a19bac06ce
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-06-04 08:57:13 +0530

    encoding user role in dev con

--- src/modules/services/permission-service.js
+++ src/modules/services/permission-service.js
@@ -1,15 +1,22 @@
 // Modules
+import base64 from "base-64";
+
+// Services
 import StorageService from "./storage-service";
 import Permissions from "../stores/permissions";
 
 // Get Role
 const getRole = () => {
-  return StorageService.getLocalData("role");
+  let role = StorageService.getLocalData("role");
+  return base64.decode(role);
 };
 
 // Set Role
 const setRole = role => {
-  if (role) return StorageService.setLocalData("role", role);
+  if (role) {
+    role = base64.encode(role);
+    return StorageService.setLocalData("role", role);
+  }
 };
 
 // Get Permissions
