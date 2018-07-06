0b6f63c51b18c45114bcd81ff699a3baa8714b96
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-07-06 21:28:15 +0530

    fixing dashbaord page

--- src/modules/components/pages/dashboard.js
+++ src/modules/components/pages/dashboard.js
@@ -4,14 +4,31 @@ import React from "react";
 // Components
 import StatCard from "../common/stat-card";
 
+// Services
+import StorageService from "../../services/storage-service";
+
 // Classes
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    // Get User Details
+    let user = StorageService.getLocalDataAsJson("user");
+    if (user) return this.setState({ user: user });
+  }
+
   render() {
+    let { user } = this.state;
     return (
       <div className="uk-padding-large">
         {/* Greeting */}
         <div className="uk-width-1-1 uk-margin-medium-bottom">
-          <h1>Hi Ajay!</h1>
+          <h1>Hi {user.firstname}!</h1>
           <p>Here's the progress you've made so far...</p>
         </div>
         {/* Cards */}
